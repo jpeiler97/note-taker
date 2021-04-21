8ad92ba10fef749d388a575c87e3bfa69f7a2e4b
JP Eiler <jp.eiler97@gmail.com>
2021-04-21 09:43:54 -0500

    added fs and route to api

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const { RSA_NO_PADDING } = require('constants');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,5 +14,6 @@ const notes = [];
 
 app.get('/', (req, res) => res.sendFile(path.resolve('../../index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.resolve('../../notes.html')));
+app.get('/api/notes', (req, res) => res.json(notes))
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
