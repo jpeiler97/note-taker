b96ded2ef2f2e6a64b1400c79aaf11a7699f138e
JP Eiler <jp.eiler97@gmail.com>
2021-04-21 10:05:53 -0500

    added more api routes and fixed db.json path

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const { RSA_NO_PADDING } = require('constants');
+var appDir = path.dirname(require.main.filename);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,11 +9,19 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const notes = [];
+console.log(path.resolve('../../../db/db.json'));
+let noteData = fs.readFileSync(path.resolve('../../../db/db.json'));
+let notes = JSON.parse(noteData);
+
 
 
 app.get('/', (req, res) => res.sendFile(path.resolve('../../index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.resolve('../../notes.html')));
 app.get('/api/notes', (req, res) => res.json(notes))
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    res.json(newNote);
+})
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
