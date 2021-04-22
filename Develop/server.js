51956529f06b9bf4f1446a1c709e0756b09f188d
JP Eiler <jp.eiler97@gmail.com>
2021-04-22 10:46:00 -0500

    notes can be accessed in the api by id

--- Develop/server.js
+++ Develop/server.js
@@ -2,13 +2,10 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 let noteData = require('./db/db.json');
-console.log("Note data from file: ", noteData);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 let notes = noteData;
-
-console.log("notes = noteData: ", notes);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,13 +14,25 @@ app.use(express.static('public'));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 app.get('/api/notes', (req, res) => res.json(noteData));
+app.get('/api/notes/:id', (req, res) => {
+	const chosen = req.params.id;
+
+	console.log(typeof chosen);
+
+
+	for (let i = 0; i < notes.length; i++) {
+        console.log("notes[i].id ", typeof notes[i].id)
+		if (chosen === (notes[i].id).toString()) {
+			return res.json(notes[i]);
+		}
+	}
+    return res.json(false);
+});
 
 app.post('/api/notes', (req, res) => {
     const newNote = req.body;
     newNote.id = notes.length + 1;
     notes.push(newNote);
-    console.log("New note: ", newNote);
-    console.log("notes after pushing newNote: ", notes);
     fs.writeFile('./db/db.json', JSON.stringify(notes), (err) =>
     err ? console.log(err) : console.log('success'));
     res.json(newNote);
