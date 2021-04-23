04eb41b926c518d5b580dc8140173418e108c8ab
JP Eiler <jp.eiler97@gmail.com>
2021-04-23 11:01:35 -0500

    small change to path to index.html

--- Develop/server.js
+++ Develop/server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 app.get('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', 'utf-8', function(err, data) {
