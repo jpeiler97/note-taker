2ca46d95d51019eda3349d7aac3ae256abcbbde9
JP Eiler <jp.eiler97@gmail.com>
2021-04-23 11:12:38 -0500

    more path changes

--- Develop/server.js
+++ Develop/server.js
@@ -9,7 +9,7 @@ let notes = noteData;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
